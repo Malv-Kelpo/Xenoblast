4099965f1953b5e95b63fa63679b69f4199a1f40
Kelsey Codilla <kcodilla05@gmail.com>
2025-07-02 00:23:32 -0700

    Main Menu
    Simple main menu that contains a start and quit button.

--- Xenoblast/Assets/Scripts/UI/PauseMenu.cs
+++ Xenoblast/Assets/Scripts/UI/PauseMenu.cs
@@ -1,8 +1,7 @@
 using UnityEngine;
 
-public class ExplosiveBullet : MonoBehaviour
+public class PauseMenu : MonoBehaviour
 {
-    //[SerializeField] private float blastRadius = 2f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {

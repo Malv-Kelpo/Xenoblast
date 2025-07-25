56045982bae3a9728fbe70e9e3717ebb9bd6a460
Kelsey Codilla <kcodilla05@gmail.com>
2025-07-25 14:35:47 -0700

    PauseMenu
    Created Pause Menu that opens when you press the escape key, Resume and Return to Main Menu methods done, still needs the Controls method.

--- Xenoblast/Assets/Scripts/UI/PauseMenu.cs
+++ Xenoblast/Assets/Scripts/UI/PauseMenu.cs
@@ -1,16 +1,49 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public GameObject pauseMenu;
+    private bool isPaused = false;
+    public void Pause(InputAction.CallbackContext context)
     {
-        
+        if (context.performed)
+        {
+            if (!isPaused)
+            {
+                PauseGame();
+            }
+            else
+            {
+                Resume();
+            }
+        }
+    }
+    public void Resume()
+    {
+        isPaused = false;
+        Time.timeScale = 1f;
+        pauseMenu.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ReturnToMainMenu()
     {
-        
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
     }
+
+    public void Controls()
+    {
+
+    }
+
+    void PauseGame()
+    {
+        isPaused = true;
+        Time.timeScale = 0f;
+        pauseMenu.SetActive(true);
+    }
+    
+
 }
